db8801d88557bb3bd7e96bb60968906a70fa422b
lucielejard <lucielejard@5c657d5f-f92f-0410-bf4e-417a11dd3c0b>
2008-12-31 15:14:05 +0000

    Products.PloneGetPaid new version 0.7.3

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '0.7.2dev'
+version = '0.7.3dev'
 
 setup(name='Products.PloneGetPaid',
       version=version,
