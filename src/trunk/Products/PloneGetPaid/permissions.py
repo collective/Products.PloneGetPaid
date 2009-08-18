23a1da304e30e268ff93474f594dd6d5b38a995e
brandon.craig.rhodes <brandon.craig.rhodes@5c657d5f-f92f-0410-bf4e-417a11dd3c0b>
2009-08-18 11:58:52 +0000

    Removed an extraneous "trunk" directory in PloneGetPaid that was created without comment by r2706.

--- src/trunk/Products/PloneGetPaid/permissions.py
+++
@@ -1,8 +0,0 @@
-from AccessControl import ModuleSecurityInfo
-from Products.CMFCore.permissions import setDefaultRoles
-
-security = ModuleSecurityInfo('Products.PloneGetPaid')
-
-ManageOrders = 'PloneGetPaid: Manage Orders'
-security.declarePublic(ManageOrders)
-setDefaultRoles(ManageOrders, ('Manager',))
