55ab00c7bf1e9dc423c5acbea1d0f7d5d339f926
brandon.craig.rhodes <brandon.craig.rhodes@5c657d5f-f92f-0410-bf4e-417a11dd3c0b>
2009-09-25 23:33:59 +0000

    Replaced the checkout-button view that I had first created in PloneGetPaid with a checkout *buttons* (plural) view that can show as many buttons as there are off-site payment processors that want the user's attention.

---
+++ Products/PloneGetPaid/browser/checkout_buttons.py
@@ -0,0 +1,69 @@
+from zope import component
+from zope.app.component.hooks import getSite
+
+#from getpaid.core.interfaces import ICheckoutWizard
+
+from Products.Five.browser import BrowserView
+from Products.PloneGetPaid.interfaces import IGetPaidManagementOptions
+from getpaid.core.interfaces import IOffsitePaymentProcessor
+from getpaid.core.processors import PaymentSituation
+
+class CheckoutButtons(BrowserView):
+    """View which renders one or more Checkout buttons.
+
+    This view presents zero, one, or more checkout buttons, depending on
+    how the store owner has configured the site.
+
+    If the store owner has activated on-site payment processing, then a
+    plain "Checkout" button is shown that leads to the first page of the
+    GetPaid checkout wizard.
+
+    If the store owner has activated an off-site payment processor that
+    needs the user sent off-site right at the beginning of the checkout
+    process, then that processor's button will appear.
+
+    If the store owner activated both on-site and off-site processing,
+    or if they activated multiple off-site processors, then multiple
+    buttons will be shown, allowing the shopper to choose.
+
+    If no processor is active, a message is displayed suggesting that
+    the store owner might want to activate one.
+
+    """
+    def __call__(self):
+        """Render our template, after computing some instance attributes.
+
+        Before asking our template to render, we figure out whether the
+        on-site checkout button should display, and also look up the
+        views for the various off-site checkout buttons that we need to
+        render.
+
+        """
+        self.show_onsite_button = False
+        self.offsite_buttons = []
+
+        siteroot = getSite()
+        manage_options = IGetPaidManagementOptions(siteroot)
+        if manage_options.allow_onsite_payment:
+            self.show_onsite_button = True
+
+        # TODO: the payment "situation" should include pp options and
+        # shopping cart, so that processors can vary their answers about
+        # whether they offer a checkout button
+
+        situation = PaymentSituation(None, None)
+        for name in sorted(manage_options.offsite_payment_processors):
+            pp = component.getAdapter(situation, IOffsitePaymentProcessor,
+                                      name=name)
+            checkout_button = pp.checkout_button
+            if checkout_button is not None:
+                view = component.getMultiAdapter((pp, self.request),
+                                                 name=checkout_button)
+                view = view.__of__(self.context) # magic that makes it work
+                self.offsite_buttons.append(view)
+            elif pp.payment_form:
+                self.show_onsite_button = True
+
+        # Render our template.
+
+        return self.index()
