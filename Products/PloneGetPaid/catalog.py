113086f1b448e388137627f0e9cf6bb9f06441c9
davide.moro@gmail.com <davide.moro@gmail.com@5c657d5f-f92f-0410-bf4e-417a11dd3c0b>
2009-10-31 16:21:34 +0000

    mo more deprecation warnings, moved to plone.indexer with backword compatibility

--- Products/PloneGetPaid/catalog.py
+++ Products/PloneGetPaid/catalog.py
@@ -4,13 +4,29 @@ from getpaid.core.interfaces import IPayable
 from Products.CMFPlone import CatalogTool
 from Products.PloneGetPaid.interfaces import IPayableMarker
 
+try:
+    # Plone 3.3*
+    from plone.indexer.decorator import indexer
 
-def get_price(object, portal, **kw):
-    if not IPayableMarker.providedBy(object):
+
+    @indexer(IPayable)
+    def get_price(object):
+        if not IPayableMarker.providedBy(object):
+            return None
+        adapted = IPayable(object, None)
+        if adapted is not None:
+            return adapted.price
         return None
-    adapted = IPayable(object, None)
-    if adapted is not None:
-        return adapted.price
-    return None
 
-CatalogTool.registerIndexableAttribute('price', get_price)
+except ImportError:
+    # Plone 3.2.x code
+
+    def get_price(object, portal, **kw):
+        if not IPayableMarker.providedBy(object):
+            return None
+        adapted = IPayable(object, None)
+        if adapted is not None:
+            return adapted.price
+        return None
+
+    CatalogTool.registerIndexableAttribute('price', get_price)
