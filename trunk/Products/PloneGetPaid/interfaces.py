9ac61114e3956e038f527147f4d257374e1ddbef
datakurre <datakurre@5c657d5f-f92f-0410-bf4e-417a11dd3c0b>
2010-04-13 15:01:46 +0000

    Removed payment settings "accepted_credit_cards" and "use_ssl_for_checkout", since they should be payment processor specific settings (and also the default on-site credit card payment should be processed by an explicit processor plugin).

--- trunk/Products/PloneGetPaid/interfaces.py
+++ trunk/Products/PloneGetPaid/interfaces.py
@@ -251,17 +251,6 @@ class IGetPaidManagementPaymentOptions( igetpaid.IPersistentOptions ):
 
     allow_anonymous_checkout = schema.Bool( title=_(u"Allow Anonymous Checkout"), default=False)
 
-    use_ssl_for_checkout = schema.Bool( title=_(u"Use SSL for checkout"), default=False)
-
-    accepted_credit_cards = schema.List(
-        title = _(u"Accepted Credit Cards"),
-        required = False,
-        default = [],
-        description = _(u"Credit cards accepted for payment"),
-        value_type = schema.Choice( title=u"credit_card_types",
-                                    source="getpaid.core.credit_card_types" )
-        )
-
 # Order Management
 class IGetPaidManagementCustomerInformation( igetpaid.IPersistentOptions ):
     """
