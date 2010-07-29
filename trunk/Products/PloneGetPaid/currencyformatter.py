a9e72b6aabf5843f8e4135d67501b49aad517205
datakurre <datakurre@5c657d5f-f92f-0410-bf4e-417a11dd3c0b>
2010-07-29 14:27:47 +0000

    Experimental CurrencyFormatter for formatting prices by settings and localization.

--- trunk/Products/PloneGetPaid/currencyformatter.py
+++ trunk/Products/PloneGetPaid/currencyformatter.py
@@ -1,19 +1,42 @@
-from Products.PloneGetPaid import interfaces, vocabularies
+"""
+Currency Formatter Utility
+"""
+
+__version__ = "$Revision$"
+# $Id$
+# $URL$
+
+from zope import component
+
+from Products.PloneGetPaid.interfaces import IGetPaidManagementCurrencyOptions
+
+from zope.i18n import translate
 
 class CurrencyFormatter(object):
-    
+
+    def format(self, context, request, value):
+        portal_state = component.getMultiAdapter((context, request), name=u'plone_portal_state')
+        language = portal_state.language()
+
+        decimal_symbol = translate("decimal_symbol", domain="Products.PloneGetPaid", default=",",
+                         target_language = language)
+
+        return translate("formatted_price", domain="Products.PloneGetPaid", default="${value} ${currency}",
+                         mapping = {"value": ("%0.2f" % value).replace(".", decimal_symbol),
+                                    "currency": self.currency_symbol },
+                         target_language = language)
+    @property
+    def precision(self):
+        ### FIXME: Why this field on IGetPaidManagementCurrencyOptions is TextLine?
+        options = component.queryUtility(IGetPaidManagementCurrencyOptions)
+        return int(options.digits_after_decimal)
+
+    @property
+    def currency_symbol(self):
+        options = component.queryUtility(IGetPaidManagementCurrencyOptions)
+        return options.currency_symbol
+
+    # legacy method
     def currency(self, context):
-        """
-        @return: Formatted price text
-        """
-        
-        portal = context.portal_url.getPortalObject()
-        
-
-        # 1. read the setting
-        options = interfaces.IGetPaidManagementOptions(portal)
-
-        # 2. format the string
-        currency = options.currency_symbol
-
-        return currency
+        """ returns currency symbol """
+        return self.currency_symbol
