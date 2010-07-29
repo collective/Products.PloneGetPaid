b480188108dd793da623a4c62612cf1b3cfa05cf
datakurre <datakurre@5c657d5f-f92f-0410-bf4e-417a11dd3c0b>
2010-07-29 14:36:02 +0000

    Replaced viewlet approach on payment processor settings with z3c.form based on.

--- trunk/Products/PloneGetPaid/browser/admin_processors.py
+++ trunk/Products/PloneGetPaid/browser/admin_processors.py
@@ -1,74 +1,128 @@
 """
-admin payment processor settings
-
-$Id: admin_processors.py 3449 2010-04-13 14:46:04Z datakurre $
+Payment Processor Options Form
 """
 
-from Products.Five.browser.pagetemplatefile import ZopeTwoPageTemplateFile
-from Products.Five.browser import BrowserView
-from Products.Five.formlib import formbase
-from Products.Five.viewlet import manager
-from Products.PloneGetPaid import interfaces
+__version__ = "$Revision$"
+# $Id$
+# $URL$
 
-from zope.formlib import form
+from zope import component, event, interface, lifecycleevent
 
-from Products.PloneGetPaid.interfaces import ISettingsPaymentManager
-from Products.PloneGetPaid.i18n import _
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from Products.CMFCore.utils import getToolByName
 
-from base import FormViewlet
-from widgets import SelectWidgetFactory
-from admin import SettingViewletManager, PluginSettingsManagerTemplate
+from z3c.form import field, group, form
+from z3c.form.interfaces import IFormLayer, IGroup 
 
+from getpaid.core import interfaces
 
-class _PaymentViewletManager( SettingViewletManager ):
-
-    def _getNames( self ):
-        options = interfaces.IGetPaidManagementPaymentOptions( self.context )
-        return options.payment_processors
+from Products.PloneGetPaid.interfaces import IGetPaidManagementOptions, IGetPaidManagementPaymentOptions
+from Products.PloneGetPaid.browser.interfaces import IDontShowGetPaidPortlets
+from Products.PloneGetPaid import _
 
 
-PaymentViewletManager = manager.ViewletManager( "PaymentViewletManager",
-                                                ISettingsPaymentManager,
-                                                PluginSettingsManagerTemplate,
-                                                bases=(_PaymentViewletManager,)
-                                                )
+class PaymentProcessorOptionsBase(group.Group):
+    """ Base sheet for payment processor options """
 
-class PaymentOptions( BrowserView ):
+    def getContent(self):
+        return component.getUtility(interfaces.IPaymentProcessor, name=self.prefix)
+
+
+class EnabledPaymentProcessors(PaymentProcessorOptionsBase):
+    """ Fixed sheet to enable and disable installed payment processor """
+    prefix = "enabled_processors"
+    label = _(u"Enabled Processors")
+    weight = 0
+    fields = field.Fields(IGetPaidManagementPaymentOptions)
+
+    def getContent(self):
+        site = getToolByName(self.context, 'portal_url').getPortalObject()
+        return IGetPaidManagementOptions(site)
+
+
+class IPaymentProcessorOptions(interface.Interface):
+    """ Marker interface for procsessor options """
+
+
+class PaymentProcessorOptionsForm(group.GroupForm, form.EditForm):
+    """ 
+    Options form for enabling and configuring installed payment
+    processor plugins.
+
+    FIXME: This and LineItemCointainerEditForm cointain a lot of
+    similar code. Especially extractData() and applyChanges(). 
+    A common base class should be created
     """
-    container view for all payment options (payment plugin config ui gets
-    pulled in to this view as well).
-    """
-    template = ZopeTwoPageTemplateFile('templates/settings-processors.pt')
+    interface.implements(IPaymentProcessorOptions,
+                         IDontShowGetPaidPortlets)
+    groups = None
+    fields = field.Fields()
 
-    def __call__( self ):
-        return self.template()
+    render = ViewPageTemplateFile("templates/settings-processors.pt")
 
+    def __init__(self, context, request):
+        super(PaymentProcessorOptionsForm, self).__init__(context, request)
+        # Adds IFormLayer for request to support z3c.form
+        interface.alsoProvides(self.request, IFormLayer)
 
-class PaymentProcessors( FormViewlet, formbase.EditForm ):
-    """
-    viewlet for selecting payment options
-    """
-    form_fields = form.Fields(interfaces.IGetPaidManagementPaymentOptions)
-    form_fields['payment_processors'].custom_widget = SelectWidgetFactory
-    form_name = _(u'Payment Options')
+    def updateGroups(self):
+        site = getToolByName(self.context, 'portal_url').getPortalObject()
+        enabled_processors = sorted(IGetPaidManagementOptions(site).payment_processors)
 
-    template = ZopeTwoPageTemplateFile('templates/form.pt')
+        if self.groups is None \
+                or sorted([g.prefix for g in self.groups]) != enabled_processors:
+            groups = []
+            for name, group in component.getAdapters((self.context, self.request, self), IGroup):
+                if name in enabled_processors or name == "enabled_processors":
+                    groups.append(group)
+            self.groups = tuple(sorted(groups, lambda x,y: cmp(int(getattr(x, 'weight', 100)), int(getattr(y, 'weight', 100)))))
 
-    def setUpWidgets( self, ignore_request=False ):
-        self.adapters = self.adapters or {}
-        self.widgets = form.setUpEditWidgets(
-            self.form_fields, self.prefix, self.context, self.request,
-            adapters=self.adapters, ignore_request=ignore_request
-            )
+        for group in self.groups:
+            group.update()
+                
+    def update(self):
+        self.updateWidgets()
+        self.updateGroups()
+        self.updateActions()
+        self.actions.execute()
+        if self.refreshActions:
+            self.updateActions()
+        self.updateGroups() # enabled processors may have changed
 
-    def update( self ):
-        try:
-            interface = iter( self.form_fields ).next().field.interface
-        except StopIteration:
-            interface = None
-        if interface is not None:
-            self.adapters = { interface : interfaces.IGetPaidManagementOptions( self.context ) }
-        super( PaymentProcessors, self).update()
+    def extractData(self, setErrors=True):
+        """See interfaces.IEditForm and group.GroupForm's implementation """
+        data, errors = form.EditForm.extractData(self, setErrors=setErrors)
+        for group in self.groups:
+            groupData, groupErrors = group.extractData(setErrors=setErrors)
+            data.update({group.prefix: groupData}) # (!)
+            if groupErrors:
+                if errors:
+                    errors += groupErrors
+                else:
+                    errors = groupErrors
+        return data, errors
 
-    def render( self ):
-        return self.template()
+    def applyChanges(self, data):
+        """See interfaces.IEditForm and group.GroupForm's implementation """
+        changes = {}
+        for group in self.groups:
+            groupData = data.has_key(group.prefix) and data[group.prefix] or {}
+            groupChanges = group.applyChanges(groupData) # (!)
+            groupContent = group.getContent()
+            for interface, names in groupChanges.items():
+                if not changes.has_key(groupContent):
+                    changes[groupContent] = {}
+                changes[groupContent][interface] = changes[groupContent].get(interface, []) + names
+        if changes:
+            for groupContent, groupChanges in changes.items():
+                descriptions = []
+                for interface, names in groupChanges.items():
+                    descriptions.append(
+                        lifecycleevent.Attributes(interface, *names))
+                # Send out a detailed object-modified event for every modified object
+                event.notify(
+                    lifecycleevent.ObjectModifiedEvent(groupContent,
+                        *descriptions))
+        # content = self.getContent()
+        # changes.update(form.applyChanges(self, content, data))
+        return changes
