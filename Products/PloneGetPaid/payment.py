899dd2ce3fb2895246c7a3e27ee18b80185a7314
dglick <dglick@5c657d5f-f92f-0410-bf4e-417a11dd3c0b>
2011-06-21 21:42:06 +0000

    add unnamed IPaymentProcessor adapter factory to get the default processor

--- Products/PloneGetPaid/payment.py
+++ Products/PloneGetPaid/payment.py
@@ -1,6 +1,9 @@
+from zope.component import adapter, queryAdapter
 from zope.interface import implements
 from getpaid.core.payment import CreditCardTypeEnumerator
 from getpaid.core.interfaces import ICreditCardTypeEnumerator
+from getpaid.core.interfaces import IStore
+from getpaid.core.interfaces import IPaymentProcessor
 from interfaces import IGetPaidManagementOptions
 from Products.CMFCore.utils import getToolByName
 
@@ -12,6 +15,16 @@ class CreditCardTypeEnumerator(CreditCardTypeEnumerator):
 
     def acceptedCreditCardTypes(self):
         # Get the configured values
-        portal = getToolByName(self.context.context, 'portal_url').getPortalObject()
+        context = getattr(self.context, 'context', self.context)
+        portal = getToolByName(context, 'portal_url').getPortalObject()
         options = IGetPaidManagementOptions(portal)
         return options.accepted_credit_cards
+
+
+@adapter(IStore)
+def DefaultPaymentProcessorAdapterFactory(store):
+    manage_options = IGetPaidManagementOptions(store)
+    processor_name = manage_options.payment_processor
+    if not processor_name:
+        raise ValueError("No Payment Processor Specified")
+    return processor_name, queryAdapter(store, IPaymentProcessor, processor_name)
