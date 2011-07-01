30960370c26af2fce1e4527eb404ecc8c18be25b
dglick@gmail.com <dglick@gmail.com@5c657d5f-f92f-0410-bf4e-417a11dd3c0b>
2011-07-01 00:46:58 +0000

    vb

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '0.10.4'
+version = '0.10.5dev'
 
 setup(name='Products.PloneGetPaid',
       version=version,
