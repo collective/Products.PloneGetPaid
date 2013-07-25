d17111cbaab04f2ca71991ee7f4ac4dfbf40db15
Laurent Lasudry <lasudry@gmail.com>
2013-07-25 13:21:22 +0200

    Fix re-enabling of inputs after unchecking ship_same_billing checkbox

--- Products/PloneGetPaid/browser/resources/checkout-process.js
+++ Products/PloneGetPaid/browser/resources/checkout-process.js
@@ -2,14 +2,14 @@ jQuery(function($) {
 
     /* ship_same_billing enables/disables all inputs of #mailing-address */
     $("#form\\.ship_same_billing").bind('click', function(e) {
-
-        var $disable = '';
         if ($(this).attr('checked')) {
-            $disable = 'disabled';
+            $("#mailing-address input").attr("disabled", "disabled");
+            $("#mailing-address select").attr("disabled", "disabled");
         }
-        $("#mailing-address input").attr("disabled", $disable);
-        $("#mailing-address select").attr("disabled", $disable);
-        $(this).removeAttr('disabled');
+        else {
+            $("#mailing-address input").removeAttr("disabled");
+            $("#mailing-address select").removeAttr("disabled");
+        }
     });
 
 });
