6a64099e186230b79065b26ea4ec3d446257f29a
Nathan Van Gheem <vangheem@gmail.com>
2013-09-11 17:11:42 -0500

    Prepare Products.PloneGetPaid 0.10.5.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '0.10.5dev'
+version = '0.10.5'
 
 setup(name='Products.PloneGetPaid',
       version=version,
